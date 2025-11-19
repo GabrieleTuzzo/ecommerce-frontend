4ba4287751adbfd6456f46a025c722c24120b6e6
Gabriele Tuzzo <gabriele.tuzzo@gmail.com>
2025-11-19 14:36:11 +0100

    ADD setup env file and minor changes

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import productsSlice from "./productsSlice.js";
 
 const store = configureStore({
   reducer: {
     // Add your reducers here
+    products: productsSlice.reducer,
   },
 });
 
