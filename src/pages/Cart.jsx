78ab7a82cd909c8a9cff219deb90ad3e74ddd6db
Gabriele Tuzzo <gabriele.tuzzo@gmail.com>
2025-11-24 17:21:48 +0100

    ADD empty cart button from cart page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,14 +1,24 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { clearCart } from "../store/cartSlice";
 import CartCard from "../components/CartCard";
 
 export default function Cart() {
   const cartArray = useSelector((state) => state.cart.items);
   const cartTotal = useSelector((state) => state.cart.total);
+  const dispatch = useDispatch();
 
   return (
     <div className="grid grid-cols-3 gap-4">
       <div className="col-span-2">
-        <h1 className="text-3xl font-bold mb-5">My Cart</h1>
+        <div>
+          <h1 className="text-3xl font-bold mb-5">My Cart</h1>
+          <button
+            onClick={() => dispatch(clearCart())}
+            className="btn btn-primary"
+          >
+            Empty Cart
+          </button>
+        </div>
         <div className="overflow-x-auto">
           {cartArray.map((item, i) => (
             <CartCard key={i} fullSize={true} {...item} />
