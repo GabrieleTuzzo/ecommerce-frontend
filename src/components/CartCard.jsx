e4c0c4ca164ab76af8ae2d30502681fac563dc57
Gabriele Tuzzo <gabriele.tuzzo@gmail.com>
2025-11-24 17:17:31 +0100

    ADD Cart page logic and CartCard

--- src/components/CartCard.jsx
+++ src/components/CartCard.jsx
@@ -1,7 +1,7 @@
 import { useDispatch } from "react-redux";
 import { addItem, removeItem, removeAllItem } from "../store/cartSlice";
 
-export default function CartCard({ ...item }) {
+export default function CartCard({ fullSize = false, ...item }) {
   const dispatch = useDispatch();
 
   const handleAddItem = () => {
@@ -15,6 +15,39 @@ export default function CartCard({ ...item }) {
   const handleRemoveAllItem = () => {
     dispatch(removeAllItem(item));
   };
+
+  if (fullSize) {
+    return (
+      <div className="card card-lg bg-base-100">
+        <div className="card-body">
+          <div className="card-title flex justify-between">
+            <h2>{item.name}</h2>
+            {item.quantity > 1 && (
+              <div className="text-center">x{item.quantity}</div>
+            )}
+          </div>
+          <div className="card-actions justify-between items-center">
+            <p className="font-semibold text-xl">{item.price}€</p>
+
+            <div className="gap-1 flex">
+              <button
+                onClick={handleRemoveAllItem}
+                className="btn btn-secondary"
+              >
+                Remove All
+              </button>
+              <button onClick={handleRemoveItem} className="btn btn-secondary">
+                -
+              </button>
+              <button onClick={handleAddItem} className="btn btn-secondary">
+                +
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className="card card-xs bg-base-100">
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,3 +1,28 @@
+import { useSelector } from "react-redux";
+import CartCard from "../components/CartCard";
+
 export default function Cart() {
-  return <h1>cart page</h1>;
+  const cartArray = useSelector((state) => state.cart.items);
+  const cartTotal = useSelector((state) => state.cart.total);
+
+  return (
+    <div className="grid grid-cols-3 gap-4">
+      <div className="col-span-2">
+        <h1 className="text-3xl font-bold mb-5">My Cart</h1>
+        <div className="overflow-x-auto">
+          {cartArray.map((item, i) => (
+            <CartCard key={i} fullSize={true} {...item} />
+          ))}
+        </div>
+        <div className="flex justify-between text-xl">
+          <span className="font-semibold">Subtotal:</span>
+          <span>{cartTotal.toFixed(2)}€</span>
+        </div>
+      </div>
+      <div>
+        <h1 className="text-2xl font-bold mb-5">Order section</h1>
+        <button className="btn btn-primary">Order Now!</button>
+      </div>
+    </div>
+  );
 }
